6c7b061599513df4883da0600471162ec0731df3
adityaraj-iiitl <adityarajmn203@gmail.com>
2026-07-01 17:49:08 +0530

    Flask - QR code generator
    Created a qr code generator app using flask backend

--- app.py
+++ app.py
@@ -1,61 +1,15 @@
-from flask import Flask, redirect, render_template, request 
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import Flask, render_template , request , send_file
+import qrcode as qrcode
 
+app = Flask(__name__)
 
-app= Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///users.db'
-db=SQLAlchemy(app)
-
-class User(db.Model):
-    sno = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(200), nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    datetime = db.Column(db.DateTime, default=datetime.utcnow)
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    all_users=User.query.all()
-    return render_template("index.html" , all_users=all_users)
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/register" , methods=["GET" , "POST"])
-def register():
-    if request.method== "POST":
-        username=request.form.get("username")
-        password=request.form.get("password")
-        print(username)
-        new_user=User(username=username, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-    return render_template("form.html")
-
-@app.route("/update/<sno>", methods=["GET", "POST"])
-def update(sno):
-    user = User.query.filter_by(sno=int(sno)).first()
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        if user:
-            user.username = username
-            user.password = password
-            db.session.commit()
-        return redirect("/")
-        
-    return render_template("update.html", user=user)
-      
-@app.route("/delete/<sno>")
-def delete(sno):
-    user = User.query.filter_by(sno=int(sno)).first()
-    if(user):
-        db.session.delete(user)
-        db.session.commit()
-    return redirect("/")
-        
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+    if(request.method=="POST"):
+        text=request.form.get("text")
+        qrcode.make(text).save(".png")
+        return send_file(".png", mimetype="image/png")
+    return render_template("index.html")
+            
+if (__name__ == "__main__"):
     app.run(debug=True)
